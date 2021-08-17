fcf1c54c7d1b81fcc008feaff8ca361e997b6c47
leslieyi <leslieyi411@gmail.com>
2021-08-17 19:19:18 -0400

    sleep

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
+# write your code here
 
-def countdown
+def countdown(i)
+  i = 10
+  while i > 0
+    puts "#{i} SECOND(S)!" 
+    i -= 1
 end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(i)
+    i = 10
+    while i > 0
+      puts "#{i} SECOND(S)!" 
+      sleep(1) #sleep(num_secs)
+      i -= 1
+  end
+  "HAPPY NEW YEAR!"
+end 
